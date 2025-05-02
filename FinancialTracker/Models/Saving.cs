a96764045ceead627526f6e24cac50eecdf94f07
l4z4r <l4z4rdev@gmail.com>
2025-05-02 22:55:18 +0200

    Added a Savings page, Info page as well as everything that comes with that (TransactionService modification, added models, changed css)

---
+++ FinancialTracker/Models/Saving.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using SQLite;
+
+namespace FinancialTracker.Models
+{
+    public class Saving
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Unesite naziv")]
+        [System.ComponentModel.DataAnnotations.MaxLength(50, ErrorMessage = "Naziv štednje ne može biti duže od 50 karaktera")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Unesite iznos")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Iznos mora biti veći od 0")]
+        public decimal TargetAmount { get; set; }
+
+        public decimal CurrentAmount { get; set; } = 0;
+
+        public bool IsCompleted => CurrentAmount >= TargetAmount;
+    }
+}
